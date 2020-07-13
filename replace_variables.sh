c1cb9793ca11c818cb6ef8fac931cbc661cecb42
kevin Martínez <kevinccbsg@gmail.com>
2020-07-13 23:07:41 +0200

    feat: add replace keys script

---
+++ replace_variables.sh
@@ -0,0 +1,9 @@
+
+
+sed -i -e "s/PROJECT_CREDENTIAL/$PROJECT_CREDENTIAL/g" terraform.tfvars
+sed -i -e "s/PROJECT_ID/$PROJECT_ID/g" terraform.tfvars
+sed -i -e "s/PROJECT_REGION/$PROJECT_REGION/g" terraform.tfvars
+
+## It is creating this file so I added to remove after
+#it is created
+rm terraform.tfvars-e
