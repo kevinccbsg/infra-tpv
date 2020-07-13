cc8f1a0f7f8e92df080ee6ac9477c58a73f37098
kevin Martínez <kevinccbsg@gmail.com>
2020-07-14 00:53:57 +0200

    feat: add backend configuration

--- replace_variables.sh
+++ replace_variables.sh
@@ -2,6 +2,7 @@
 
 sed -i -e "s/PROJECT_CREDENTIAL/$PROJECT_CREDENTIAL/g" terraform.tfvars
 sed -i -e "s/PROJECT_ID/$PROJECT_ID/g" terraform.tfvars
+sed -i -e "s/PROJECT_ID/$PROJECT_ID/g" backend.tf
 sed -i -e "s/PROJECT_REGION/$PROJECT_REGION/g" terraform.tfvars
 
 ## It is creating this file so I added to remove after
