616ec1f89138c2cf78e01d16b4b760eb62329955
kevin Martínez <kevinccbsg@gmail.com>
2020-07-15 00:42:25 +0200

    fix: module source

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "google" {
   region      = var.project_region
 }
 
-module "ke" {
-  project = var.project_id
-  region  = var.project_region
+module "gke" {
+  source         = "./gke"
+  project_region = var.project_region
 }
