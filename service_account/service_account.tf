404b7e8f51899616bfa547947fad0d54a5920a24
kevin Martínez <kevinccbsg@gmail.com>
2020-07-23 01:43:35 +0200

    feat: remove policies

--- service_account/service_account.tf
+++ service_account/service_account.tf
@@ -3,38 +3,3 @@ resource "google_service_account" "sa_deployment" {
   account_id   = "service-account-tpv"
   display_name = "A service account for Github actions deployment"
 }
-
-resource "google_project_iam_binding" "viewer-project-iam" {
-  role               = "roles/viewer"
-  members = [
-    "serviceAccount:${google_service_account.sa_deployment.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "kubernetes-engine-developer-project-iam" {
-  role               = "roles/container.developer"
-  members = [
-    "serviceAccount:${google_service_account.sa_deployment.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "serviceaccountuser-project-iam" {
-  role               = "roles/iam.serviceAccountUser"
-  members = [
-    "serviceAccount:${google_service_account.sa_deployment.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "storage-admin-project-iam" {
-  role               = "roles/storage.admin"
-  members = [
-    "serviceAccount:${google_service_account.sa_deployment.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "cloud-run-admin-project-iam" {
-  role               = "roles/run.admin"
-  members = [
-    "serviceAccount:${google_service_account.sa_deployment.email}"
-  ]
-}
