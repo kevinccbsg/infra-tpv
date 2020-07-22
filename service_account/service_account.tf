9c93ba530ff9fa4fd8332e3c112593347bf06867
kevin Martínez <kevinccbsg@gmail.com>
2020-07-23 01:24:13 +0200

    fix: service account

--- service_account/service_account.tf
+++ service_account/service_account.tf
@@ -4,40 +4,35 @@ resource "google_service_account" "sa_deployment" {
   display_name = "A service account for Github actions deployment"
 }
 
-resource "google_service_account_iam_binding" "viewer-iam" {
-  service_account_id = google_service_account.sa_deployment.name
+resource "google_project_iam_binding" "viewer-project-iam" {
   role               = "roles/viewer"
   members = [
     "serviceAccount:${google_service_account.sa_deployment.email}"
   ]
 }
 
-resource "google_service_account_iam_binding" "kubernetes-engine-developer-iam" {
-  service_account_id = google_service_account.sa_deployment.name
+resource "google_project_iam_binding" "kubernetes-engine-developer-project-iam" {
   role               = "roles/container.developer"
   members = [
     "serviceAccount:${google_service_account.sa_deployment.email}"
   ]
 }
 
-resource "google_service_account_iam_binding" "serviceaccountuser-iam" {
-  service_account_id = google_service_account.sa_deployment.name
+resource "google_project_iam_binding" "serviceaccountuser-project-iam" {
   role               = "roles/iam.serviceAccountUser"
   members = [
     "serviceAccount:${google_service_account.sa_deployment.email}"
   ]
 }
 
-resource "google_service_account_iam_binding" "storage-admin-iam" {
-  service_account_id = google_service_account.sa_deployment.name
+resource "google_project_iam_binding" "storage-admin-project-iam" {
   role               = "roles/storage.admin"
   members = [
     "serviceAccount:${google_service_account.sa_deployment.email}"
   ]
 }
 
-resource "google_service_account_iam_binding" "cloud-run-admin-iam" {
-  service_account_id = google_service_account.sa_deployment.name
+resource "google_project_iam_binding" "cloud-run-admin-project-iam" {
   role               = "roles/run.admin"
   members = [
     "serviceAccount:${google_service_account.sa_deployment.email}"
