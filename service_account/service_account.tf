2ac77d6d260e3d4bf3ae668bd32640c60db31578
kevin Martínez <kevinccbsg@gmail.com>
2020-07-23 01:08:18 +0200

    fix: service account members

--- service_account/service_account.tf
+++ service_account/service_account.tf
@@ -7,29 +7,39 @@ resource "google_service_account" "sa_deployment" {
 resource "google_service_account_iam_binding" "viewer-iam" {
   service_account_id = google_service_account.sa_deployment.name
   role               = "roles/viewer"
-  member             = "serviceAccount:${google_service_account.sa_deployment.email}"
+  members = [
+    "serviceAccount:${google_service_account.sa_deployment.email}"
+  ]
 }
 
 resource "google_service_account_iam_binding" "kubernetes-engine-developer-iam" {
   service_account_id = google_service_account.sa_deployment.name
   role               = "roles/container.developer"
-  member             = "serviceAccount:${google_service_account.sa_deployment.email}"
+  members = [
+    "serviceAccount:${google_service_account.sa_deployment.email}"
+  ]
 }
 
 resource "google_service_account_iam_binding" "serviceaccountuser-iam" {
   service_account_id = google_service_account.sa_deployment.name
   role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:${google_service_account.sa_deployment.email}"
+  members = [
+    "serviceAccount:${google_service_account.sa_deployment.email}"
+  ]
 }
 
 resource "google_service_account_iam_binding" "storage-admin-iam" {
   service_account_id = google_service_account.sa_deployment.name
   role               = "roles/storage.admin"
-  member             = "serviceAccount:${google_service_account.sa_deployment.email}"
+  members = [
+    "serviceAccount:${google_service_account.sa_deployment.email}"
+  ]
 }
 
 resource "google_service_account_iam_binding" "cloud-run-admin-iam" {
   service_account_id = google_service_account.sa_deployment.name
   role               = "roles/run.admin"
-  member             = "serviceAccount:${google_service_account.sa_deployment.email}"
+  members = [
+    "serviceAccount:${google_service_account.sa_deployment.email}"
+  ]
 }
