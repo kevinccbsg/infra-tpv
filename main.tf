2969bf75906975d2d8156353a236fc707c54618d
kevin Martínez <kevinccbsg@gmail.com>
2020-07-29 19:58:33 +0200

    feat: remove gke cluster

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "google" {
   region      = var.project_region
 }
 
-module "gke" {
-  source         = "./gke"
-  project_region = var.project_region
-}
+# module "gke" {
+#   source         = "./gke"
+#   project_region = var.project_region
+# }
 
 module "service_account" {
   source = "./service_account"
